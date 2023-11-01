73e8fa7191c0a3df4ec3bb127de88b10ed8544e1
Davit Meparishvili <Davit.Meparishvili@space.ge>
2023-11-01 20:15:48 +0400

    Add CharacterFactoryTest.

---
+++ RPGTest.UnitTests/CharacterFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using RPGClash.Domain.Characters;
+using RPGClash.GameEngine.Factory.Abstract;
+using RPGClash.GameEngine.Factory.Concrete;
+
+namespace RPGClash.UnitTests
+{
+    public class CharacterFactoryTest
+    {
+        private readonly ICharacterFactory _characterFactory;
+        
+        public CharacterFactoryTest()
+        {
+            _characterFactory = new CharacterFactory();
+        }
+
+        [Theory]
+        [InlineData(CharacterName.Cedrick)]
+        [InlineData(CharacterName.Guladi)]
+        [InlineData(CharacterName.Zezva)]
+        [InlineData(CharacterName.Lucia)]
+        public void Factory_CorrectCaracterIsCreated(CharacterName character)
+        {
+            // Arrange - Act
+            var implementation = _characterFactory.CreateCharacter(character);
+
+            //Assert
+            implementation.Name.Should().Be(character);
+        }
+    }
+}
